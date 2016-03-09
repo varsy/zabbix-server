5dcdb2c62023077a289604a16054bbc2c629aca3
Andrey Sizov <andrey.sizov@jetbrains.com>
2016-03-09 16:52:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# docker-zabbix-server
+
+This is docker image with Zabbix 3.0 server + web UI.
+
+Docker image is located at [varsy/zabbix-server](https://hub.docker.com/r/varsy/zabbix-server/).
+
+There are following environment variables you need to set:
+* `DB_HOST` 
+* `DB_USER`
+* `DB_PASS`
---
+++ run-services.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+rccheck() {
+   if [ $? != 0 ]; then
+      echo "Error! Stopping the script."
+      exit 1
+   fi
+}
+
+# check if db is created
+if ! mysql -h${DB_HOST} -u${DB_USER} -p${DB_PASS} -e 'use zabbix'; then
+   echo -n "DB is not created, trying to populate it... "
+   mysql -h${DB_HOST} -u${DB_USER} -p${DB_PASS} -e 'create database zabbix character set utf8 collate utf8_bin' ; rccheck
+   zcat /usr/share/doc/zabbix-server-mysql/create.sql.gz | mysql -h${DB_HOST} -u${DB_USER} -p${DB_PASS} zabbix ; rccheck
+   echo "ok!"
+fi
+
+sed -i "s/^.*DBHost=localhost.*$/DBHost=${DB_HOST}/" /etc/zabbix/zabbix_server.conf
+sed -i "s/^DBUser=zabbix$/DBUser=${DB_USER}/" /etc/zabbix/zabbix_server.conf
+sed -i "s/^.*DBPassword=$/DBPassword=${DB_PASS}/" /etc/zabbix/zabbix_server.conf
+
+sed -i "s/# php_value date.timezone.*/php_value date.timezone Europe\/Moscow/" /etc/zabbix/apache.conf
+
+if [ ! -f /etc/zabbix/web/zabbix.conf.php ]; then
+   cp -p /usr/share/zabbix/conf/zabbix.conf.php.example /etc/zabbix/web/zabbix.conf.php
+   sed -i "s/\$DB\['SERVER'\].*/\$DB\['SERVER'\] = '${DB_HOST}';/" /etc/zabbix/web/zabbix.conf.php
+   sed -i "s/\$DB\['PORT'\].*/\$DB\['PORT'\] = '3306';/" /etc/zabbix/web/zabbix.conf.php
+   sed -i "s/\$DB\['USER'\].*/\$DB\['USER'\] = '${DB_USER}';/" /etc/zabbix/web/zabbix.conf.php
+   sed -i "s/\$DB\['PASSWORD'\].*/\$DB\['PASSWORD'\] = '${DB_PASS}';/" /etc/zabbix/web/zabbix.conf.php
+fi
+
+trap "service apache2 stop; service zabbix-server stop" SIGINT SIGTERM SIGHUP
+service zabbix-server start
+service apache2 start
+
+tail -qF /var/log/zabbix/zabbix_server.log &
+wait
